ebbab1f798819f50d201ecc7b11df108a2eff4e2
Jefferson Marques <98566026+mrqssjeff@users.noreply.github.com>
2022-03-16 17:52:23 -0300

    exclusão

--- pythonparatodos/desafiotabuada.py
+++
@@ -1,5 +0,0 @@
-c = 0
-num = int(input('Digite um número para ver sua tabuada: '))
-while c < 11:
-    print(f'{c} x {num} = {c * num}')
-    c += 1
--- pythonparatodos/desafiovalordoproduto.py
+++
@@ -1,3 +0,0 @@
-parcela = float(input('Qual o valor da parcela do produto? R$ '))
-meses = int(input('Quantidade de meses a ser pago: '))
-print(f'O valor da compra é R${parcela * meses:.2f}')
